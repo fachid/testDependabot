24e2eb8b784ffbe3e3169f87e67b3c160aec4abd
Dionysis Fachiridis <78432513+fachid@users.noreply.github.com>
2021-11-18 15:17:07 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, send_from_directory
+from flask import Response
+from flask_restful import reqparse, abort, Api, Resource
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
+import os
+from cassandra.cluster import Cluster
+import json
+import datetime
+
+@app.route("/")
+def hello():
+    print(request.headers)
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
